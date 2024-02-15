76a84c3a1dc77767a0eda3798043c20df3a4eac5
cleibsonsilva94 <cleibsonslima@gmail.com>
2024-02-15 19:35:51 -0300

    Inicio do projeto com primeira possibilidade de interação

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RegistrodeClientesHotelCs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Registro de clientes do Hotel Cs");
+            Console.WriteLine("Seja muito bem vindo! Escolha uma das opções abaixo digitando o número dela:");
+            Console.WriteLine("1. Registrar novo cliente");
+            Console.WriteLine("2. Remover cliente");
+            Console.WriteLine("3. Pesquisar Cliente");
+            int Opção = int.Parse(Console.ReadLine());
+
+            if (Opção == 1)
+            {
+                Console.WriteLine("Nome do cliente:");
+                string nameCliente = Console.ReadLine();
+                Console.WriteLine("Idade:");
+                int Idade = int.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o número de dias que ficará no hotel:");
+                int dias = int.Parse(Console.ReadLine());
+                Console.WriteLine("Quarto que o cliente ficará:");
+                int quarto = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Cliente, {nameCliente}, registrado com sucesso! \nEle tem {Idade} anos e vai ficar {dias} dias no hotel.\nEle ficará no quarto {quarto}");
+            }
+        }
+    }
+}
